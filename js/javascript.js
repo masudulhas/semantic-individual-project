9e672f89cf4fec40beb299ab9fe635d072323941
masudulhas <masudul.has@gmail.com>
2017-12-18 03:47:50 +0100

    first commit and add some features

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,34 @@
-$(document).ready(function(){
+/* smooth scroll*/
+$(document).ready(function()
+{
+	applyClickEvent();
+	mobile();
+  closeNav();
+});
+
+	function applyClickEvent()
+	{
+	$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+});
+}
+
+/*Mobile nav*/
+function mobile(){
     $('.menu').click(function(){
         $('ul').toggleClass('active');
-    })
-})
+    });
+    // closeNav();
+  }
+  /*closeNav*/
+  function closeNav(){
+    // $('.container').click(function(){
+  	// 		 $('.nav').toggle();
+    //    });
+  }
